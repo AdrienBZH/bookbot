9a72de12d34eb96de722db995587ecfecf2b8b48
Adrien Nicolas <103822035+AdrienBZH@users.noreply.github.com>
2025-09-29 10:47:37 +0200

    print content of frankenstein.txt

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as file:
+        file_content = file.read()
+        return file_content
+    
+
+def main():
+    relative_path = "books/frankenstein.txt"
+    book_text = get_book_text(relative_path)
+    print(book_text)
+
+main()
+
+
 
 
 
