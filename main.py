42f15b1757fb2e0a79f729e04a3ecd86d92889d0
Adrien Nicolas <103822035+AdrienBZH@users.noreply.github.com>
2025-09-29 13:58:41 +0200

    using sys.argv for file_path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import count_words
 from stats import count_characters
 from stats import sort_dictionary
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -9,14 +10,18 @@ def get_book_text(filepath):
     
 
 def main():
-    relative_path = "books/frankenstein.txt"
-    book_text = get_book_text(relative_path)
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>") 
+        sys.exit(1)
+        
+    book_text = get_book_text(sys.argv[1])
     word_count = count_words(book_text)
     character_count = count_characters(book_text)
     sorted_characters = sort_dictionary(character_count, character_count)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {relative_path}...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
