420070d75d33e514aab04e3746869a3883ddcac7
Adrien Nicolas <103822035+AdrienBZH@users.noreply.github.com>
2025-09-29 11:04:23 +0200

    print character count in frankenstein.txt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -10,7 +10,10 @@ def main():
     relative_path = "books/frankenstein.txt"
     book_text = get_book_text(relative_path)
     word_count = count_words(book_text)
+    character_count = count_characters(book_text)
+    
     print(f"Found {word_count} total words")
+    print(f"{character_count}")
 
 main()
 
--- stats.py
+++ stats.py
@@ -2,6 +2,22 @@ def count_words(text):
     words = text.split()
     return len(words)
 
+def count_characters(text):
+    dict_char_count = {}
+    
+    characters = text.lower()
+
+    for character in characters:
+        if character.isalpha():
+            if character in dict_char_count:
+                character_lowercase = character.lower()
+                dict_char_count[character_lowercase] += 1
+            else:
+                dict_char_count[character] = 1
+
+    return dict_char_count
+
+
 
 
 
